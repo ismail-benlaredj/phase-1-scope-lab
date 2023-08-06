c0df7fe8a586b1e80cc8d2ad81040461d1d2e9d5
ismail <yako_is@yahoo.fr>
2023-08-06 17:07:22 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+function upperCaseCustomerName() {
+    customerName = customerName.toLocaleUpperCase()
+}
+function setBestCustomer() {
+    globalThis.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(string) {
+    bestCustomer = string
+}
+
+const leastFavoriteCustomer = "constant"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "change"
+}
